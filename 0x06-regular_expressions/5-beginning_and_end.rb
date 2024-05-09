52bede93f57e92386b01b96a2bb77f42a30411f1
Zainab990 <zainabomar998@gmail.com>
2024-05-09 05:50:39 -0700

    repare 5

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,16 +1,3 @@
 #!/usr/bin/env ruby
-# Get the first command-line argument
-input_string = ARGV[0]
-
-# Define a regular expression pattern
-pattern = /^h.*n$/
-
-# Find all matches of the pattern in the input string
-matches = input_string.scan(pattern)
-
-# Join the matched substrings into a single string
-result = matches.join
-
-# Print the result
-puts result
+puts ARGV[0].scan(/^h.n$).join
 
