d60f8075fd38bc502c87323c8bafb3ef2e209d9a
Zainab990 <zainabomar998@gmail.com>
2024-05-09 04:23:38 -0700

    5th task

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,18 +1,18 @@
 #!/usr/bin/env ruby
 
-# Define a method to check for the pattern in a string 5
-def check_pattern(str)
-  pattern = /^h.n$/  # Regular expression pattern
-  if str.match?(pattern)  # Check if the string matches the pattern
-    puts "The string '#{str}' matches the pattern!"
-  else
-    puts "The string '#{str}' does not match the pattern!"
-  end
-end
-
-# Accept the first command-line argument as the input string
+# Get the first command-line argument
 input_string = ARGV[0]
 
-# Call the method with the input string
-check_pattern(input_string)
+# Define a regular expression pattern
+pattern = /^h.*n/
+
+# Find all matches of the pattern in the input string
+matches = input_string.scan(pattern)
+
+# Join the matched substrings into a single string
+result = matches.join
+
+# Print the result
+puts result
+
 
