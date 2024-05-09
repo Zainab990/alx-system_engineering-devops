d604e060f3f5da5a0d64640d1ef813d1d4c6a06f
Zainab990 <zainabomar998@gmail.com>
2024-05-09 04:44:52 -0700

    matching phone number

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -3,8 +3,8 @@
 # Get the first command-line argument
 input_string = ARGV[0]
 
-# Define a regular expression pattern to match numbers followed by 'n'
-pattern = /^[0-9]+n$/
+# Define a regular expression pattern to match a 10-digit phone number
+pattern = /^[0-9]{10}$/
 
 # Find all matches of the pattern in the input string
 matches = input_string.scan(pattern)
