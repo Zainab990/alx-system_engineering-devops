b5c039ffebc98a0d5319ff19bfec17b0b5e090ad
Zainab990 <zainabomar998@gmail.com>
2024-05-09 05:36:12 -0700

    repare 1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,12 +1,4 @@
 #!/usr/bin/env ruby
+# task 1
+puts ARGV[0].scan(/hbt[2,5]n/).join
 
-input_string = ARGV[0]
-pattern = /hbt{2,5}n/  # Matches "hbtn", "hbttn", "hbtttn", "hbttttn", "hbtttttn"
-
-if input_string.match?(pattern)
-  puts input_string
-else
-  puts "No match"
-end
-
-puts ""  # Ensure the script ends with a new line
