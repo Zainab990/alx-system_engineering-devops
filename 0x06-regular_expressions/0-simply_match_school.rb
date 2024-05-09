c998869ffbd49fbc235e309ddaaa86728c01433c
Zainab990 <zainabomar998@gmail.com>
2024-05-09 05:28:46 -0700

    repare 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,23 +1,3 @@
 #!/usr/bin/env ruby
-
-# Check if the user provided a command-line argument
-if ARGV.empty?
-  puts "Usage: ./find_school.rb 'input_string'"
-  exit
-end
-
-# Get the input string from the command-line argument
-input_string = ARGV[0]
-
-# Define a regular expression pattern to find occurrences of "school"
-pattern = /school/
-
-# Find all occurrences of "school" in the input string
-matches = input_string.scan(pattern)
-
-# Join the matched substrings into a single string
-result = matches.join
-
-# Output the result
-puts result
+puts ARGV[0].scan(/school/).join
 
